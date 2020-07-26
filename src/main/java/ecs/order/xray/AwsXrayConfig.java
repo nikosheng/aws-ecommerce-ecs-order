5d498f86566ee3817f940fefb9dafb96d9476e19
Niko <niko.js.feng@gmail.com>
2020-07-26 15:20:39 +0800

    Initial commit

---
+++ src/main/java/ecs/order/xray/AwsXrayConfig.java
@@ -0,0 +1,16 @@
+package ecs.order.xray;
+
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class AwsXrayConfig {
+
+    @Bean
+    public Filter TracingFilter() {
+        return new AWSXRayServletFilter("eks-order");
+    }
+}
